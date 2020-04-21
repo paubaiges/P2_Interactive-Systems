202dd2ccdcefc57be3e5669fd555ceea8b30e4ec
paubaiges <59828767+paubaiges@users.noreply.github.com>
2020-04-21 15:45:26 +0200

    Add files via upload

---
+++ Assets/RW/Scripts/MoveHayBale.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveHayBale : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Vector3 movementSpeed; //1
+    public Space space; //2
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(movementSpeed * Time.deltaTime, space);
+
+    }
+}
---
+++ Assets/RW/Scripts/MovingHay.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingHay : MonoBehaviour
+{
+    public float limitHayMov = 22;
+    public GameObject hayPrefab;
+    public GameObject hayBalePrefab; 
+    public Transform haySpawnpoint; 
+    public float shootInterval; 
+    private float shootTimer; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalInput < 0 && transform.position.x > -limitHayMov )
+        {
+            transform.Translate(-transform.right * 10 * Time.deltaTime);
+        }
+        if (horizontalInput > 0 && transform.position.x < limitHayMov)
+        {
+            transform.Translate(transform.right * 10 * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(hayPrefab, transform.position, Quaternion.identity);
+        }
+        UpdateShooting();
+
+    }
+
+    private void UpdateShooting()
+    {
+        shootTimer -= Time.deltaTime; // 1
+
+        if (shootTimer <= 0 && Input.GetKey(KeyCode.Space)) // 2
+        {
+            shootTimer = shootInterval; // 3
+            ShootHay(); // 4
+        }
+    }
+
+    private void ShootHay()
+    {
+        Instantiate(hayBalePrefab, haySpawnpoint.position, Quaternion.identity);
+    }
+
+}
